e3fe2317d29d7e118e40ec5b362224272b6271fd
Kevin Smyth <kevin.m.smyth@gmail.com>
2015-12-15 11:54:07 -0600

    Dont modify testbench_manifest.jsons while running test

--- run_mdao/test/__init__.py
+++ run_mdao/test/__init__.py
@@ -9,11 +9,23 @@ import subprocess
 
 _this_dir = os.path.dirname(os.path.abspath(__file__))
 
-for input_filename in glob.glob(_this_dir + '/mdao_config*json'):
-    os.chdir(_this_dir)
-    run_mdao.run(input_filename)
-    output_filename = input_filename + '.output.csv'
-    shutil.copyfile('output.csv', output_filename)
-    changed = subprocess.check_output('git diff --name-only'.split() + [output_filename])
-    if len(changed) > 0:
-        raise Exception(changed)
+
+def main():
+    tb_json_names = glob.glob(os.path.join(_this_dir, '*/testbench_manifest.json'))
+    tb_jsons = [(tb_json_name, open(tb_json_name, 'rb').read()) for tb_json_name in tb_json_names]
+    try:
+        for input_filename in glob.glob(_this_dir + '/mdao_config*json'):
+            os.chdir(_this_dir)
+            run_mdao.run(input_filename)
+            output_filename = input_filename + '.output.csv'
+            shutil.copyfile('output.csv', output_filename)
+            changed = subprocess.check_output('git diff --name-only'.split() + [output_filename])
+            if len(changed) > 0:
+                raise Exception(changed)
+    finally:
+        for tb_json_name, contents in tb_jsons:
+            with open(tb_json_name, 'wb') as tb_json:
+                tb_json.write(contents)
+
+if __name__ == '__main__':
+    main()
