db4ce30cac64ec9355691be63c0339aa84315aed
Kevin Smyth <kevin.m.smyth@gmail.com>
2016-05-06 10:13:28 -0500

    Fix merge error

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
     author_email='ksmyth@metamorphsoftware.com',
     description='Runs a mdao_config.json with OpenMDAO',
     packages=['run_mdao', 'run_mdao.python_component', 'run_mdao.python_component.get_params_and_unknowns'],
-    packages=['run_mdao', 'run_mdao.python_component'],
     package_dir={'run_mdao': 'run_mdao'},
     entry_points={
         "console_scripts": [
