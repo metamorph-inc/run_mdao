b6be9be20a29c3a9222d823680ab7f44a6355c18
Kevin Smyth <kevin.m.smyth@gmail.com>
2016-08-04 13:52:34 -0500

    v0.1.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='run_mdao',
-    version='0.1.6',
+    version='0.1.7',
     author='MetaMorph Software, Inc',
     author_email='ksmyth@metamorphsoftware.com',
     description='Runs a mdao_config.json with OpenMDAO',
