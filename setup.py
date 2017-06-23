3eb08dd6391d554922ed9c57a63fc7ccf4515fac
Kevin Smyth <kevin.m.smyth@gmail.com>
2017-06-23 12:45:19 -0500

    Version bump 0.1.11

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='run_mdao',
-    version='0.1.10',
+    version='0.1.11',
     author='MetaMorph Software, Inc',
     author_email='ksmyth@metamorphsoftware.com',
     description='Runs a mdao_config.json with OpenMDAO',
